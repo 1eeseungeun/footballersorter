b9f282b071b99589d050f79431784edca037ae11
1eeseungeun <141208182+1eeseungeun@users.noreply.github.com>
2024-09-10 10:12:28 +0900

    update the ac milan squad

--- ac-milan/sort/js/acmilan-squad.js
+++ ac-milan/sort/js/acmilan-squad.js
@@ -1,205 +1,191 @@
 const players = [
   {
-    name: "Maignan",
-    img: "https://img.legaseriea.it/vimages/64e49678/MGNMK.png?webp&q=70&ch=840&size=477x-",
+    name: "Mike Maignan",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/6f30d75d-f476-421a-bb17-4dd79c2259c5/Maignan-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "goalkeeper" ]
       }
   },
     {
-    name: "Sportiello",
-    img: "https://img.legaseriea.it/vimages/64e3881f/SPRMX.png?webp&q=70&ch=840&size=477x-",
+    name: "Marco Sportiello",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/9dcbfac9-35c4-4dc3-92ef-c10a6a06508f/Sportiello-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "goalkeeper" ]
       }
   },
     {
-    name: "Mirante",
-    img: "https://img.legaseriea.it/vimages/64e38848/MRNNT.png?webp&q=70&ch=840&size=477x-",
+    name: "Lorenzo Torriani",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/804cd370-8197-4398-8afc-dfcade477c42/Torriani-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "goalkeeper" ]
       }
   },
     {
-    name: "Calabria",
-    img: "https://img.legaseriea.it/vimages/64e38810/VBVMP.png?webp&q=70&ch=840&size=477x-",
+    name: "Davide Calabria",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/e928f98a-fb6c-4903-be29-664460023ad9/Calabria-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "defender" ]
       }
   },
     {
-    name: "Hernández",
-    img: "https://img.legaseriea.it/vimages/64e4968c/HRNZTH.png?webp&q=70&ch=840&size=477x-",
+    name: "Alessandro Florenzi",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/b579a585-4a38-41be-8c0f-342afe5d2d29/Florenzi-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "defender" ]
       }
   },
     {
-    name: "Kalulu",
-    img: "https://img.legaseriea.it/vimages/64e49687/KLLPRR.png?webp&q=70&ch=840&size=477x-",
+    name: "Matteo Gabbia",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/8c9217d5-1a36-4551-ac9b-29f15e081041/Gabbia-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "defender" ]
       }
   },
     {
-    name: "Tomori",
-    img: "https://img.legaseriea.it/vimages/64e3882f/TMRFKY.png?webp&q=70&ch=840&size=477x-",
+    name: "Theo Hernández",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/12daf100-2931-4851-a437-a1707ec03a0c/TheoHernandez-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "defender" ]
       }
   },
     {
-    name: "Kjær",
-    img: "https://img.legaseriea.it/vimages/64e49682/KJRSM.png?webp&q=70&ch=840&size=477x-",
+    name: "Theo Hernández",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/12daf100-2931-4851-a437-a1707ec03a0c/TheoHernandez-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "defender" ]
       }
   },
     {
-    name: "Thiaw",
-    img: "https://img.legaseriea.it/vimages/64e3881a/THWLCK.png?webp&q=70&ch=840&size=477x-",
+    name: "Alejandro Jiménez Sánchez",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/ca9273b8-1b60-40ef-8c65-606e4967576f/Jimenez-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "defender" ]
       }
   },
     {
-    name: "Caldara",
-    img: "https://img.legaseriea.it/vimages/6512ef48/LTXVC.png?webp&q=70&ch=840&size=477x-",
+    name: "Strahinja Pavlović",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/2047d678-47d3-458a-958d-eb37399517f3/Pavlovic-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "defender" ]
       }
   },
     {
-    name: "Florenzi",
-    img: "https://img.legaseriea.it/vimages/64e3882a/FLRLN.png?webp&q=70&ch=840&size=477x-",
+    name: "Emerson Royal",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/7bbcb168-87b1-48b8-a24a-2f251e9b193f/ERoyal-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "defender" ]
       }
   },
     {
-    name: "Gabbia",
-    img: "https://img.legaseriea.it/vimages/659bea3f/GBBMTT.png?webp&q=70&ch=840&size=477x-",
+    name: "Filippo Terracciano",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/f695e1bb-8fc0-441f-9fcc-8e58bd87aea0/Terracciano-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "defender" ]
       }
   },
     {
-    name: "Jimenez",
-    img: "https://img.legaseriea.it/vimages/659bea49/JMNNDR.png?webp&q=70&ch=840&size=477x-",
+    name: "Malick Thiaw",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/685dfb41-ac3d-4306-878c-dd08a964cc1d/Thiaw-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "defender" ]
       }
   },
     {
-    name: "Simic",
-    img: "https://img.legaseriea.it/vimages/656ef6f2/SMCCRL.png?webp&q=70&ch=840&size=477x-",
+    name: "Fikayo Tomori",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/cbe97042-4d71-4e72-81b7-4925315d0c32/Tomori-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "defender" ]
       }
   },
     {
-    name: "Bartesaghi",
-    img: "https://img.legaseriea.it/vimages/659bea44/BRTDV1.png?webp&q=70&ch=840&size=477x-",
-    opts: {
-      position: [ "defender" ]
-      }
-  },
-    {
-    name: "Bennacer",
-    img: "https://img.legaseriea.it/vimages/656ef6f6/BNNSML.png?webp&q=70&ch=840&size=477x-",
+    name: "Ismaël Bennacer",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/468cddff-852b-4bf1-a2d4-eb091f0169b9/Bennacer-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "midfielder" ]
       }
   },
     {
-    name: "Adli",
-    img: "https://img.legaseriea.it/vimages/64e496a0/ADLIYA.png?webp&q=70&ch=840&size=477x-",
+    name: "Youssouf Fofana",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/e1901035-9048-433c-a772-385ea0523180/Fofana-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "midfielder" ]
       }
   },
     {
-    name: "Loftus-Cheek",
-    img: "https://img.legaseriea.it/vimages/64e38843/LFTRB.png?webp&q=70&ch=840&size=477x-",
-    opts: {
-      position: [ "midfielder" ]
-      }
-  },
-     {
-    name: "Pulisic",
-    img: "https://img.legaseriea.it/vimages/64e38852/RCNWX.png?webp&q=70&ch=840&size=477x-",
+    name: "Ruben Loftus-Cheek",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/9fca4d86-70b9-4c84-b546-b3fda7a9d82c/LoftusCheek-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "midfielder" ]
       }
   },
     {
-    name: "Reijnders",
-    img: "https://img.legaseriea.it/vimages/64e38834/RJNJJN.png?webp&q=70&ch=840&size=477x-",
+    name: "Yunus Musah",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/4b7023f0-63b6-4947-94f7-3c78b1975ba8/Musah-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "midfielder" ]
       }
   },
     {
-    name: "Chukwueze",
-    img: "https://img.legaseriea.it/vimages/64e496a5/CHKRNK.png?webp&q=70&ch=840&size=477x-",
+    name: "Christian Pulisic",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/b73b4067-0d1c-447f-be62-311c6c7c1112/Pulisic-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "midfielder" ]
       }
   },
     {
-    name: "Pobega",
-    img: "https://img.legaseriea.it/vimages/64e38839/PBGMMS.png?webp&q=70&ch=840&size=477x-",
+    name: "Tijjani Reijnders",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/3598fc37-4834-4dcb-813f-e9522fec0def/Reijnders-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "midfielder" ]
       }
   },
     {
-    name: "Terracciano",
-    img: "https://img.legaseriea.it/vimages/65a265a5/TRRFIL.png?webp&q=70&ch=840&size=477x-",
-    opts: {
-      position: [ "midfielder" ]
-      }
-  },
-     {
-    name: "Musah",
-    img: "https://img.legaseriea.it/vimages/64e4969b/MSHYNS.png?webp&q=70&ch=840&size=477x-",
+    name: "Kevin Zeroli",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/d8ed9769-13a0-445c-a080-0c326c88ccad/Zeroli-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "midfielder" ]
       }
   },
     {
-    name: "Giroud",
-    img: "https://img.legaseriea.it/vimages/64e49691/GRDLV.png?webp&q=70&ch=840&size=477x-",
+    name: "Tammy Abraham",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/54056435-380d-498c-8df2-177d5ae65d2a/Abraham-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "forward" ]
       }
   },
     {
-    name: "Leão",
-    img: "https://img.legaseriea.it/vimages/64e4967d/LEAORA.png?webp&q=70&ch=840&size=477x-",
+    name: "Samuel Chukwueze",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/fdde0760-356c-4b70-bc02-e04db827db5d/Chukwueze-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "forward" ]
       }
   },
     {
-    name: "Jovic",
-    img: "https://img.legaseriea.it/vimages/6512ef4d/JOVICL.png?webp&q=70&ch=840&size=477x-",
+    name: "Luka Jović",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/e6b0f13d-0922-47b7-9146-2def3a1bffa9/Jovic-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "forward" ]
       }
   },
     {
-    name: "Okafor",
-    img: "https://img.legaseriea.it/vimages/64e49696/OKAFOR.png?webp&q=70&ch=840&size=477x-",
+    name: "Rafael Leão",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/34e66e33-7612-4995-bb7e-e51f5e6e2ecb/Leao-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "forward" ]
       }
   },
     {
-    name: "Camarda",
-    img: "https://img.legaseriea.it/vimages/6560e984/CMRCSC.png?webp&q=70&ch=840&size=477x-",
+    name: "Álvaro Morata",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/80bfdaf4-b13b-4738-82ee-551ecdab04d2/Morata-Large.png?w=undefined&h=498&dpr=1&auto=format",
+    opts: {
+      position: [ "forward" ]
+      }
+  },
+    {
+    name: "Noah Okafor",
+    img: "https://assets-eu-01.kc-usercontent.com/1293c890-579f-01b7-8480-902cca7de55e/d6fea093-045c-4c38-9758-5441d0873d20/Okafor-Large.png?w=undefined&h=498&dpr=1&auto=format",
     opts: {
       position: [ "forward" ]
       }
   }
-]; // 2024-07-19
+]; // 2024-09-10
